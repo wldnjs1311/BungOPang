01e6c978a9d5841c2d0d44331949419e4f5ee9d7
wldnjs1311 <wldnjs1311@naver.com>
2024-04-08 16:24:35 +0900

    Match3 - Pop 구현

--- Assets/Scripts/TycoonScene/Match3/Board.cs
+++ Assets/Scripts/TycoonScene/Match3/Board.cs
@@ -1,6 +1,7 @@
+using DG.Tweening;
 using System.Collections.Generic;
 using System.Linq;
-using TMPro;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Board : MonoBehaviour
@@ -16,12 +17,14 @@ public class Board : MonoBehaviour
 
     private readonly List<Tile> selection_ = new List<Tile>();
 
-    private const float TweenDuration = 0.25f;
+    private const float TweenDuration = 0.125f;
 
     private Tile selectedTile1_;
     private Tile selectedTile2_;
 
     private void Awake() => Instance = this;
+
+    public List<Tile> popTiles;
 
     private void Start()
     {
@@ -39,21 +42,35 @@ public class Board : MonoBehaviour
                 tile.Item = ItemDatabase.Items[Random.Range(0, ItemDatabase.Items.Length)];
 
                 Tiles[x, y] = tile;
-
             }
         }
+
+        SetPop();
+        Pop();
     }
-    public void Select(Tile tile)
+    public async void Select(Tile tile)
     {
-        if(!selection_.Contains(tile)) selection_.Add(tile);
+        if (selection_.Count() == 0) selection_.Add(tile);
+        else if (selection_[0].NeighboursX.Contains(tile) || selection_[0].NeighboursY.Contains(tile))
+            selection_.Add(tile);
+        else selection_.Clear();
+
         if (selection_.Count < 2) return;
 
-        Debug.Log(message: $"Selected tiles at ({selection_[0].x}, {selection_[0].y}) and ({selection_[1].x}, {selection_[1].y})");
+        await Swap(selection_[0], selection_[1]);
+
+        if (CanPop(selection_[0], selection_[1]))
+        {
+            SetPop();
+            Pop();
+        }
+        else
+            await Swap(selection_[0], selection_[1]);
 
         selection_.Clear();
     }
 
-    public async Task<> Swap(Tile tile1, Tile tile2)
+    public async Task Swap(Tile tile1, Tile tile2)
     {
         var icon1 = tile1.icon_;
         var icon2 = tile2.icon_;
@@ -62,24 +79,89 @@ public class Board : MonoBehaviour
         var icon2Transform = icon2.transform;
 
         //선택한 두 개 위치 바꾸기
-
-        //만약 바꿨을 때 match 안되면 원래대로
         var sequence = DOTween.Sequence();
 
         sequence.Join(icon1Transform.DOMove(icon2Transform.position, TweenDuration))
             .Join(icon2Transform.DOMove(icon1Transform.position, TweenDuration));
 
-        await sequence.Play() //sequence
-            .AsyncWaitForCompletion(); //task
+        await sequence.Play() //실행
+            .AsyncWaitForCompletion();  //한 task 끝날 때까지 기다리기
 
         //부모 변경
         icon1Transform.SetParent(tile2.transform);
         icon2Transform.SetParent(tile1.transform);
 
+        //tile 아이콘 변경
         tile1.icon_ = icon2;
         tile2.icon_ = icon1;
 
-        var title1Item = tile1.Item;
+        var tile1Item = tile1.Item;
+
+        //아이템 변경
+        tile1.Item = tile2.Item;
+        tile2.Item = tile1Item;
     }
 
+    private void Update()
+    {
+
+    }
+
+    private bool CanPop(Tile a, Tile b)
+    {
+        if(a.GetConnectedTileX().Skip(1).Count() >= 2 || a.GetConnectedTileY().Skip(1).Count() >= 2 
+            || b.GetConnectedTileX().Skip(1).Count() >= 2 || b.GetConnectedTileY().Skip(1).Count() >= 2)
+            return true;
+        return false;
+    }
+
+    private void SetPop()
+    {
+        for (var y = 0; y < Height; y++)
+            for (var x = 0; x < Width; x++)
+            {
+                if (popTiles.Contains(Tiles[x,y])) continue;
+
+                if (Tiles[x, y].GetConnectedTileX().Skip(1).Count() >= 2)
+                    foreach (Tile tile in Tiles[x, y].GetConnectedTileX())
+                        popTiles.Add(tile);
+
+                if (Tiles[x, y].GetConnectedTileY().Skip(1).Count() >= 2)
+                    foreach (Tile tile in Tiles[x, y].GetConnectedTileY())
+                        popTiles.Add(tile);
+            }
+    }
+
+    private async void Pop()
+    {
+        var deflateSequence = DOTween.Sequence();
+
+        foreach(var tile in popTiles)
+            deflateSequence.Join(tile.icon_.transform.DOScale(Vector3.zero, TweenDuration));
+
+        await deflateSequence.Play()
+            .AsyncWaitForCompletion();
+
+        var inflateSequence = DOTween.Sequence();
+
+        foreach (var tile in popTiles) //새로운 아이템 생성
+        {
+            tile.Item = ItemDatabase.Items[Random.Range(0, ItemDatabase.Items.Length)];
+            inflateSequence.Join(tile.icon_.transform.DOScale(Vector3.one, TweenDuration));
+
+        }
+        await inflateSequence.Play()
+            .AsyncWaitForCompletion();
+
+        popTiles.Clear();
+    }
 }
+
+//최적화
+//CanPop() 을 원소 하나씩 확인하는걸로 바꿔서 pop된 원소에 대해서만 검사
+
+/*
+ * 해결해야 할 문제
+ * 1. pop한 후 새로 생성된 item들의 match는 터지지 않음
+ * 2. 더 이상 맞출 수 없는 puzzle의 경우 rearrange 필요
+ */
--- Assets/Scripts/TycoonScene/Match3/Tile.cs
+++ Assets/Scripts/TycoonScene/Match3/Tile.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Tile : MonoBehaviour
+public sealed class Tile : MonoBehaviour
 {
     public int x;
     public int y;
@@ -25,8 +26,69 @@ public class Tile : MonoBehaviour
     public Image icon_;
     public Button button_;
 
-    private void Start()
+    public Tile Left => x > 0 ? Board.Instance.Tiles[x - 1, y] : null;
+    public Tile Top => y > 0 ? Board.Instance.Tiles[x, y - 1] : null;
+    public Tile Right => x < Board.Instance.Width - 1 ? Board.Instance.Tiles[x + 1, y] : null;
+    public Tile Bottom => y < Board.Instance.Height - 1 ? Board.Instance.Tiles[x, y + 1] : null;
+
+    public Tile[] NeighboursX => new[]
     {
-        button_.onClick.AddListener(call: () => Board.Instance.Select(tile: this));
+        Left,
+        Right,
+    };
+
+    public Tile[] NeighboursY => new[]
+    {
+        Top,
+        Bottom,
+    };
+
+    private void Start() => button_.onClick.AddListener(call: () => Board.Instance.Select(tile: this));
+
+    public List<Tile> GetConnectedTileX(List<Tile> exclude = null)
+    {
+        var result = new List<Tile> { this, };
+
+        if (exclude == null)
+        {
+            exclude = new List<Tile> { this, };
+        }
+        else
+        {
+            exclude.Add(this);
+        }
+
+        foreach (var neighbour in NeighboursX)
+        {
+            if (neighbour == null || exclude.Contains(neighbour) || neighbour.Item != Item) continue;
+
+            result.AddRange(neighbour.GetConnectedTileX(exclude));
+        }
+
+        return result;
+    }
+
+    public List<Tile> GetConnectedTileY(List<Tile> exclude = null)
+    {
+        //세로로 이어져 있는 타일 반환
+        var result = new List<Tile> { this, };
+
+        if (exclude == null)
+        {
+            exclude = new List<Tile> { this, };
+        }
+        else
+        {
+            exclude.Add(item: this);
+        }
+
+        foreach (var neighbour in NeighboursY)
+        {
+            if (neighbour == null || exclude.Contains(neighbour) || neighbour.Item != Item) continue;
+
+            result.AddRange(collection: neighbour.GetConnectedTileY(exclude));
+        }
+
+        return result;
     }
 }
