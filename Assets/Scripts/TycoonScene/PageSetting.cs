c2f6a122909a1b103d053002f1e2911caec8e206
wldnjs1311 <wldnjs1311@naver.com>
2024-04-10 16:56:17 +0900

    PageSetting 보류

--- Assets/Scripts/TycoonScene/PageSetting.cs
+++ Assets/Scripts/TycoonScene/PageSetting.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class PageSetting : MonoBehaviour
 {
@@ -6,11 +7,36 @@ public class PageSetting : MonoBehaviour
 
     private void Start()
     {
+        if(childPages != null)
+        {
+            SetChildTransform();
+            //SetChildSize();
+        }
+    }
+
+    private void SetChildTransform()
+    {
         var cameraHeight = Camera.main.orthographicSize * 2;
         var cameraWidth = cameraHeight * Camera.main.aspect;
 
         childPages[0].transform.position = new Vector3(-cameraWidth, 0, 0);
         childPages[1].transform.position = Vector3.zero;
         childPages[2].transform.position = new Vector3(cameraWidth, 0, 0);
+    }
+    
+    private void SetChildSize()
+    {
+        var cameraHeight = Camera.main.orthographicSize * 2;
+        var cameraWidth = cameraHeight * Camera.main.aspect;
+
+        foreach (var page in childPages)
+        {
+            var scaleFactorX = cameraWidth / page.transform.localScale.x;
+            var scaleFactorY = cameraHeight / page.transform.localScale.y;
+            var scaleFactor = Mathf.Max(scaleFactorX, scaleFactorY);
+
+            transform.localScale = new Vector3(scaleFactor, scaleFactor, 1);
+        }
+
     }
 }
